e3f7e212b3f79fc203a0ba8067abb86d14e6cd62
Aijaz-siddique <Aijazsiddique20@gmail.com>
2024-12-08 03:47:58 +0530

    Create Sliding_Window.cs

---
+++ HandyCode/CodeForces/Sliding_Window.cs
@@ -0,0 +1,18 @@
+//Useful for problems involving contiguous subarrays.
+
+int[] array = {1, 2, 3, 4, 5};
+int k = 3; // Window size
+int windowSum = 0;
+
+for (int i = 0; i < k; i++) {
+    windowSum += array[i];
+}
+
+int maxSum = windowSum;
+
+for (int i = k; i < array.Length; i++) {
+    windowSum += array[i] - array[i - k];
+    maxSum = Math.Max(maxSum, windowSum);
+}
+
+Console.WriteLine($"Maximum sum of {k}-length subarray is {maxSum}");
