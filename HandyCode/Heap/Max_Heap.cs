d7c74b951a87aaa835c2e5c0d508ef8a17e89f64
Aijaz-siddique <Aijazsiddique20@gmail.com>
2024-12-08 04:24:30 +0530

    Create Max_Heap.cs

---
+++ HandyCode/Heap/Max_Heap.cs
@@ -0,0 +1,20 @@
+namespace Teamplate
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            //maxHeap
+            var maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+
+
+
+
+        }
+
+
+    }
+
+}
