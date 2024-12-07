32d6991364c0c190fe161c34e1a2df015afec382
Aijaz-siddique <Aijazsiddique20@gmail.com>
2024-12-08 03:37:01 +0530

    Create Common_DataStructures.cs

---
+++ HandyCode/CodeForces/Common_DataStructures.cs
@@ -0,0 +1,18 @@
+class Program
+{
+    static void Main()
+    {
+        // Arrays
+        int[] arr = new int[n];
+
+        // Lists
+        List<int> list = new List<int>();
+
+        // Dictionaries
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        // Sets
+        HashSet<int> set = new HashSet<int>();
+
+    }
+}
