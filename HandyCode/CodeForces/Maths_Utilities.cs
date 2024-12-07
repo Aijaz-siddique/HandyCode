a432272bd1461f0e00dde21eb7cec343e0d302e0
Aijaz-siddique <Aijazsiddique20@gmail.com>
2024-12-08 03:38:58 +0530

    Create Maths_Utilities.cs

---
+++ HandyCode/CodeForces/Maths_Utilities.cs
@@ -0,0 +1,25 @@
+// GCD
+int GCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// LCM
+int LCM(int a, int b) {
+    return (a / GCD(a, b)) * b;
+}
+
+// Prime checking
+bool IsPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
